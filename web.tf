f78c4111a489f746d8650f781a9f312ddaa5ada8
Jenkins <jenkins@ip-10-23-1-36.us-east-2.compute.internal>
2018-11-26 09:35:50 +0000

    modified

---
+++ web.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+ region = "us-east-2"
+}
+
+resource "aws_instance" "NAME" {
+  ami = "ami-0782e9ee97725263d"
+  instance_type = "t2.micro"
+  key_name = "fullstackkey"
+  subnet_id = "subnet-04adf818d05d0928f"
+  associate_public_ip_address = true
+  vpc_security_group_ids = ["sg-04c7ac1c35820a26c"]
+  user_data = "${file("install.sh")}"
+
+ tags {
+ Name = "Padma_test"
+}
+
+}
